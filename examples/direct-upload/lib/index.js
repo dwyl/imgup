776e18b017ff59906b110c7d183629ed0dfbd150
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-09-13 15:32:18 +0100

    adds callback to the start server function #18

--- examples/direct-upload/lib/index.js
+++ examples/direct-upload/lib/index.js
@@ -1,19 +1,20 @@
+'use strict'
+
 var Hapi = require('hapi')
 var Routes = require('./routes.js')
 var Inert = require('inert')
 
-exports.init = function (port, next) {
+exports.start = function (callback) {
   var server = new Hapi.Server()
-  server.connection({port: port})
+  server.connection({port: 8000})
   server.register([Inert, Routes], function (err) {
     if (err) {
-      return next(err)
+      return callback(err, null)
     }
 
     server.start(function (err) {
-      if (err) console.log('error starting server: ', err)
-      return next(err, server)
+      if (err) callback(err, null)
+      return callback(null, server)
     })
   })
-  module.exports = server
 }
