4b3a022100667e92cd69a2df7dab8af1ab81e4d8
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-09-13 16:58:23 +0100

    tries to test other routes #18

--- examples/direct-upload/test/server.test.js
+++ examples/direct-upload/test/server.test.js
@@ -17,7 +17,7 @@ test('checks our /s3_credentials GET endpoint', function (t) {
   })
 })
 
-test('POST request to /s3_credentials should return 404', function (t) {
+test('checks POST to /s3_credentials returns 404', function (t) {
   var options = {
     method: 'POST',
     url: '/s3_credentials'
@@ -27,9 +27,21 @@ test('POST request to /s3_credentials should return 404', function (t) {
       console.log(err)
     }
     server.inject(options, function (response) {
-      console.log(response.result)
       t.equal(response.statusCode, 404, '404 status code returned - ✅')
       server.stop(t.end)
     })
   })
 })
+
+// test('checks our GET request for our index.html', function (t) {
+//   Server.start((err, server) => {
+//     if (err) {
+//       console.log(err)
+//     }
+//     server.route({ method: 'GET', path: '/public/{path*}', handler: { directory: { path: './', listing: true } } })
+//     server.inject('/public/index.html', function (response) {
+//       t.equal(response.statusCode, 200, '200 status code returned - ✅')
+//       server.stop(t.end)
+//     })
+//   })
+// })
