87744c79ef1facf23a9131297005f6f44a6e3f97
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-09-27 11:44:38 +0100

    cleans up comments #15

--- examples/direct-upload/nightwatch.conf.js
+++ examples/direct-upload/nightwatch.conf.js
@@ -1,15 +1,14 @@
-// we use a nightwatch.conf.js file so we can include comments and helper functions
 module.exports = {
   'src_folders': [
     'test/e2e'// Where you are storing your Nightwatch e2e tests
   ],
   'output_folder': './reports', // reports (test outcome) output by nightwatch
-  'selenium': { // downloaded by selenium-download module (see readme)
+  'selenium': { // downloaded by selenium-download module
     'start_process': true, // tells nightwatch to start/stop the selenium process
     'server_path': '../../node_modules/nightwatch/bin/selenium.jar',
     'host': '127.0.0.1',
     'port': 4444, // standard selenium port
-    'cli_args': { // chromedriver is downloaded by selenium-download (see readme)
+    'cli_args': { // chromedriver is downloaded by selenium-download
       'webdriver.chrome.driver': '../../node_modules/nightwatch/bin/chromedriver'
     }
   },
--- examples/direct-upload/test/e2e/upload.js
+++ examples/direct-upload/test/e2e/upload.js
@@ -5,10 +5,7 @@ module.exports = {
       .waitForElementVisible('body', 1000)
       .waitForElementVisible('input#fileInput', 1000)
       .pause(1000)
-      .setValue('input#fileInput', require('path').resolve(__dirname + '/testfile.png')) // Works
-//      .setValue('input#fileUpload', 'testfile.txt') // Will not work
-//      .setValue('input#fileUpload', 'http://localhost:3000/testfile.txt') // Will not work
-//      .setValue('input[type='file']', require('path').resolve(__dirname + '/testfile.txt')) // Will not work
+      .setValue('input#fileInput', require('path').resolve(__dirname + '/testfile.png'))
       .click('#submit')
       .pause(1000)
       .assert.containsText('h4', 'Image Successfully Uploaded')
