942134dd32540729db80550f88ffa42513cc12b1
nelsonic <nelson+github@dwyl.com>
2023-06-14 03:19:46 +0100

    fix config typo #67

--- config/config.exs
+++ config/config.exs
@@ -51,8 +51,8 @@ config :logger, :console,
 config :phoenix, :json_library, Jason
 
 config :ex_aws,
-  access_key_id: System.get_env("AWS_ACCESS_KEY_ID")
-  secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY")
+  access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
+  secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
   region: System.get_env("AWS_REGION"),
   request_config_override: %{}
 
