cc0e9766b8ba98101d877ecd7069af7932b63ad3
nelsonic <nelson+github@dwyl.com>
2023-07-07 19:24:32 +0100

    add *real* test for uploading a JPEG image to S3 #91

--- test/app/upload_test.exs
+++ test/app/upload_test.exs
@@ -17,4 +17,20 @@ defmodule App.UploadTest do
 
     assert App.Upload.upload(image) == {:ok, expected_response}
   end
+
+  test "upload/1 ginger.jpg (cat pic)" do
+    image = %Plug.Upload{
+      content_type: "image/jpeg",
+      filename: "ginger.jpg",
+      path: [:code.priv_dir(:app), "static", "images", "ginger.jpg"] |> Path.join()
+    }
+
+    expected_response = %{
+      compressed_url:
+        "https://s3.eu-west-3.amazonaws.com/#{Application.get_env(:ex_aws, :compressed_bucket)}/zb2rhe5aFXPKonoWchLRYo9yJDqWyUdUeTQ6MQQJsTWnzzNum.jpg",
+      url:
+        "https://s3.eu-west-3.amazonaws.com/#{Application.get_env(:ex_aws, :original_bucket)}/zb2rhe5aFXPKonoWchLRYo9yJDqWyUdUeTQ6MQQJsTWnzzNum.jpg"
+    }
+    assert App.Upload.upload(image) == {:ok, expected_response}
+  end
 end
