e307c4d3339f555430c41edca0fb361db5f7aa85
Luís Arteiro <lucho14garfield@gmail.com>
2023-07-11 12:39:08 +0100

    fix: Refactoring pattern matches on CID and extension.

--- lib/app/upload.ex
+++ lib/app/upload.ex
@@ -19,78 +19,68 @@ defmodule App.Upload do
   """
   def upload(image) do
     with {:ok, {file_cid, file_extension}} <- check_file_binary_and_extension(image) do
-      # Check if file `cid` and extension are valid.
-      case {file_cid, file_extension} do
-        {"invalid data type", nil} ->
-          {:error, :invalid_extension_and_cid}
 
-        {"invalid data type", _extension} ->
-          {:error, :invalid_cid}
+      # Creating filename with the retrieved extension
+      file_name = "#{file_cid}.#{file_extension}"
 
-        {_cid, nil} ->
-          {:error, :invalid_extension}
+      # Upload to S3
+      request_response =
+        try do
+          image.path
+          |> ExAws.S3.Upload.stream_file()
+          |> ExAws.S3.upload(Application.get_env(:ex_aws, :original_bucket), file_name,
+            acl: :public_read,
+            content_type: image.content_type
+          )
+          |> ExAws.request(get_ex_aws_request_config_override())
+        rescue
+          _e ->
+            {:error, :upload_fail}
+        end
 
-        # If the `cid` and extension are valid, we are safe to upload
-        {file_cid, file_extension} ->
-          # Creating filename with the retrieved extension
-          file_name = "#{file_cid}.#{file_extension}"
+      # Check if the request was successful
+      case request_response do
+        # If the request was successful, we parse the result
+        {:ok, body} ->
+          # Sample response:
+          # %{
+          #   body: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n
+          #    <CompleteMultipartUploadResult xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\">
+          #    <Location>https://s3.eu-west-3.amazonaws.com/imgup-original/qvWtbC7WaT.jpg</Location>
+          #    <Bucket>imgup-original</Bucket><Key>qvWtbC7WaT.jpg</Key>
+          #    <ETag>\"4ecd62951576b7e5b4a3e869e5e98a0f-1\"</ETag></CompleteMultipartUploadResult>",
+          #   headers: [
+          #     {"x-amz-id-2",
+          #      "wNTNZKt82vgnOuT1o2Tz8z3gcRzd6wXofYxQmBUkGbBGTpmv1WbwjjGiRAUtOTYIm92bh/VJHhI="},
+          #     {"x-amz-request-id", "QRENBY1MJTQWD7CZ"},
+          #     {"Date", "Tue, 13 Jun 2023 10:22:44 GMT"},
+          #     {"x-amz-expiration",
+          #      "expiry-date=\"Thu, 15 Jun 2023 00:00:00 GMT\", rule-id=\"delete-after-1-day\""},
+          #     {"x-amz-server-side-encryption", "AES256"},
+          #     {"Content-Type", "application/xml"},
+          #     {"Transfer-Encoding", "chunked"},
+          #     {"Server", "AmazonS3"}
+          #   ],
+          #   status_code: 200
+          # }
 
-          # Upload to S3
-          request_response =
-            try do
-              image.path
-              |> ExAws.S3.Upload.stream_file()
-              |> ExAws.S3.upload(Application.get_env(:ex_aws, :original_bucket), file_name,
-                acl: :public_read,
-                content_type: image.content_type
-              )
-              |> ExAws.request(get_ex_aws_request_config_override())
-            rescue
-              _e ->
-                {:error, :upload_fail}
-            end
+          # Fetch the contents of the returned XML string from `ex_aws`.
+          # This XML is parsed with `sweet_xml`:
+          # github.com/kbrw/sweet_xml#the-x-sigil
+          url = body.body |> xpath(~x"//text()") |> List.to_string()
+          compressed_url = "#{@compressed_baseurl}#{file_name}"
+          {:ok, %{url: url, compressed_url: compressed_url}}
 
-          # Check if the request was successful
-          case request_response do
-            # If the request was successful, we parse the result
-            {:ok, body} ->
-              # Sample response:
-              # %{
-              #   body: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n
-              #    <CompleteMultipartUploadResult xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\">
-              #    <Location>https://s3.eu-west-3.amazonaws.com/imgup-original/qvWtbC7WaT.jpg</Location>
-              #    <Bucket>imgup-original</Bucket><Key>qvWtbC7WaT.jpg</Key>
-              #    <ETag>\"4ecd62951576b7e5b4a3e869e5e98a0f-1\"</ETag></CompleteMultipartUploadResult>",
-              #   headers: [
-              #     {"x-amz-id-2",
-              #      "wNTNZKt82vgnOuT1o2Tz8z3gcRzd6wXofYxQmBUkGbBGTpmv1WbwjjGiRAUtOTYIm92bh/VJHhI="},
-              #     {"x-amz-request-id", "QRENBY1MJTQWD7CZ"},
-              #     {"Date", "Tue, 13 Jun 2023 10:22:44 GMT"},
-              #     {"x-amz-expiration",
-              #      "expiry-date=\"Thu, 15 Jun 2023 00:00:00 GMT\", rule-id=\"delete-after-1-day\""},
-              #     {"x-amz-server-side-encryption", "AES256"},
-              #     {"Content-Type", "application/xml"},
-              #     {"Transfer-Encoding", "chunked"},
-              #     {"Server", "AmazonS3"}
-              #   ],
-              #   status_code: 200
-              # }
-
-              # Fetch the contents of the returned XML string from `ex_aws`.
-              # This XML is parsed with `sweet_xml`:
-              # github.com/kbrw/sweet_xml#the-x-sigil
-              url = body.body |> xpath(~x"//text()") |> List.to_string()
-              compressed_url = "#{@compressed_baseurl}#{file_name}"
-              {:ok, %{url: url, compressed_url: compressed_url}}
-
-            # If the request was unsuccessful, throw an error
-            {:error, _reason} ->
-              {:error, :upload_fail}
-          end
+        # If the request was unsuccessful, throw an error
+        {:error, _reason} ->
+          {:error, :upload_fail}
+          
       end
-
     else
       {:error, :failure_read} -> {:error, :failure_read}
+      {:error, :invalid_extension_and_cid} -> {:error, :invalid_extension_and_cid}
+      {:error, :invalid_cid} -> {:error, :invalid_cid}
+      {:error, :invalid_extension} -> {:error, :invalid_extension}
     end
   end
 
@@ -105,15 +95,27 @@ defmodule App.Upload do
           |> MIME.extensions()
           |> List.first()
 
-        # Return the file's content CID and its MIME extension
-        {:ok, {file_cid, file_extension}}
+        # Return the file's content CID and its MIME extension if valid.
+        # Otherwise, return error.
+        case {file_cid, file_extension} do
+          {"invalid data type", nil} ->
+            {:error, :invalid_extension_and_cid}
+
+          {"invalid data type", _extension} ->
+            {:error, :invalid_cid}
+
+          {_cid, nil} ->
+            {:error, :invalid_extension}
+
+          {file_cid, file_extension} ->
+            {:ok, {file_cid, file_extension}}
+        end
 
       # If image can't be opened, return error
       {:error, _reason} ->
         {:error, :failure_read}
     end
   end
-
 
   def get_ex_aws_request_config_override,
     do: Application.get_env(:ex_aws, :request_config_override)
