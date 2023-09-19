bd20a068aabaa57ed5f70039447a8deba40c6846
Nelson <194400+nelsonic@users.noreply.github.com>
2023-09-19 10:15:15 +0100

    #121 add "config :excid" to config.exs

--- config/config.exs
+++ config/config.exs
@@ -54,3 +54,6 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+# https://github.com/dwyl/cid#how
+config :excid, base: :base58
